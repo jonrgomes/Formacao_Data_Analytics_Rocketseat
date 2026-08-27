93a64bcb0e988100f1ab16dc40f1e0b43a6c2fec
jonrgomes <jonrgomes@gmail.com>
2026-08-27 19:33:58 -0300

    Fundamentos_de_Python

--- 14_strings.py
+++ 14_strings.py
@@ -1,11 +1,11 @@
-# Craindo uma String no Python
+# # Craindo uma String no Python
 nome = 'Jonatas'
 nome2 = "Gomes"
 
 texto = 'Estou aprendendo "Python"'
 print(texto)
 
-# tamanho da string
+# Tamanho da string
 print(len(nome))
 print(len(nome2))
 
@@ -16,3 +16,20 @@ print('y' in nome2)
 # Operador not in - verifica a inexistencia de um caractere e retorna um valor booleano
 print('t' not in nome)
 print('d' not in nome2)
+
+# Cadeia de caracteres - Strings são imultavei
+texto ='Python'
+print(texto[1])
+
+# Metodo capitalize
+nome = 'jose'
+nome = nome.capitalize()
+print(nome)
+
+# Deixa em caixa alta
+nome = 'mario'
+print(nome.upper())
+
+# Deixar em caixa baixa
+nome = 'MARIO'
+print(nome.lower())
