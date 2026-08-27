a1e5f64a39e6b9e949c0ef22d2570549a45bc7dc
jonrgomes <jonrgomes@gmail.com>
2026-08-26 21:12:56 -0300

    Fundamentos_de_Python

--- 14_strings.py
+++ 14_strings.py
@@ -8,3 +8,7 @@ print(texto)
 # tamanho da string
 print(len(nome))
 print(len(nome2))
+
+# Operdador in - verifica a existencia de um caractere e retorna um valor booleano
+print('a' in nome)
+print('y' in nome2)
