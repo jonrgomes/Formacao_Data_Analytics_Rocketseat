1af0537ce8f6be9a6246ec7c3c8e707bb9f4521e
jonrgomes <jonrgomes@gmail.com>
2026-08-27 19:57:33 -0300

    Fundamentos_de_Python

---
+++ 15_metodos_de_strings.py
@@ -0,0 +1,12 @@
+# Contar quantidade de vezes que um caracter aparece numa string
+email = "jonrgomes@gmail.com"
+print(email.count("a"))
+
+# Mostrar a posição de um caracter em uma string
+print(email.find(".com"))
+print(email.find("@gmail.com"))
+
+# Verifica se uma string começa com um caractere ou sequencia de caracteres especificos
+print(email.startswith("jon"))
+
+# 
