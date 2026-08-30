9cacc1c44dcba75ec373e4e815c6fb00945a5cae
jonrgomes <jonrgomes@gmail.com>
2026-08-30 19:15:52 -0300

    Fundamentos_de_Python

--- Pratica_03.py
+++ Pratica_03.py
@@ -1,10 +1,31 @@
-for a in range(1, 5, 2):
-    for b in range(a):
-        print("Mentoria do Emmersom, a melhor que está tendo!")
-        print("Professor de milhões!!!\n")
-        print("Queria te agradecer por não desistir!")
-        print("Seu Dom nos motiva a continuar!\n")
-        print("Continue a nadar!!")
-    print()
+# Nome e descoberta de idade:
 
-   
+nome = input("Qual o seu nome? ")
+idade = int(input("Qual a sua idade? "))
+ano_atual = 2026
+nova_idade = (2050 - ano_atual) + idade
+print(f"Olá, {nome}, Você tem {idade} anos e em 2050 tera {nova_idade} anos!")
+
+# Notas do Aluno
+nota = float(input("Digite a sua nota:"))
+
+if nota > 10 or nota < 0: 
+    print("Nota inválida, difite uma nota entre 0 e 10!")
+elif nota >= 9:
+    print("Sua nota é Excelente!!")
+elif nota >= 7:
+    print("Sua nota é boa!")
+elif nota >= 5:
+    print("Sua nota é regular!")
+else:
+    print("Sua nota é insuficiente!")
+
+# Laços de repetição
+valor = int(input("Digite um número: "))
+soma = 0
+
+while valor != 0:
+    print("Você digitou:", valor)
+    soma += valor
+    valor = int(input("Digite um número: "))
+print(f"A soma é {soma}!")
