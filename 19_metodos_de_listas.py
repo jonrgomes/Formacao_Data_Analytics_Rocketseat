8fe440e863028286c402d75b85bfe986b968516d
jonrgomes <jonrgomes@gmail.com>
2026-09-04 18:59:42 -0300

    Estrutura_de_dados

---
+++ 19_metodos_de_listas.py
@@ -0,0 +1,13 @@
+# Tamanho de uma listas:
+cores = ["Preto", "Azul", "Vermelho"]
+print(len(cores))
+
+# Inserindo dados em um lista (append):
+print(cores[2])
+cores.append("Liláz")
+print(cores)
+
+# Inserindo dados em uma posiçao especifíca: (insert):
+frutas = ["Goiaba", "Laranja", "Abacates"]
+frutas.insert(0, "Pera")
+print(frutas)
