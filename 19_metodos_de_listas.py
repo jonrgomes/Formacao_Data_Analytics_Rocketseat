17de5f0ef8db7c316fd1f9ea8c8d035f72258689
jonrgomes <jonrgomes@gmail.com>
2026-09-05 21:55:48 -0300

    Estrutura_de_dados

--- 19_metodos_de_listas.py
+++ 19_metodos_de_listas.py
@@ -11,3 +11,35 @@ print(cores)
 frutas = ["Goiaba", "Laranja", "Abacates"]
 frutas.insert(0, "Pera")
 print(frutas)
+
+
+
+# Juntando listas com extend:
+nome_1 = ["Ana", "Paulo", "André"]
+nome_2 = ["Pedro", "Alex", "Antonio"]
+
+nome_1.extend(nome_2)
+print(nome_1)
+
+# # Removendo dados de uma lista (remove):
+frutas = ["Laranja", "Amora", "Goiaba"]
+frutas.remove("Amora")
+
+print(frutas)
+
+# Removendo dados de uma lista (pop), esse é utilizado para remover o último elemento ou escolhe o índice escolhido.
+
+dados_1 = ["Blusa", "Camisa", "Calça"]
+dados_1.pop()
+dados_1.pop(1)
+print(dados_1)
+print(dados_1)
+
+# Removendo dados de uma lista (del):
+frutas = ["Laranja", "Amora", "Goiaba"]
+del frutas[0]
+print(frutas)
+
+# Limpando a lista do programa (del):
+frutas.clear()
+print(frutas)
