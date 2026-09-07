c71bcd70a4a1ffe89723420baa3210c5344a69f3
jonrgomes <jonrgomes@gmail.com>
2026-09-07 20:37:16 -0300

    Estrutura_de_dados

--- 21_listas_aninhadas.py
+++ 21_listas_aninhadas.py
@@ -1,2 +1,23 @@
 # Criando uma lista aninhada:
 
+colaboradores = [
+    ["Nome", "Cargo", "Departamento"],
+    ["Alice", "Analista", "Financeiro"],
+    ["Carlos", "Gerente", "Vendas"],
+    ["Julia", "Estagiaria", "RH"],
+]
+print(colaboradores)
+
+
+# Acessando um intem em listas aninhadas:
+print(colaboradores[2][2])
+
+# Modificando um elemento da lista aninhada:
+colaboradores[1][1] = "Cordenador"
+print(colaboradores[1])
+
+# Percorrendo uma lista aninhada:
+for lista in colaboradores:
+    for dados in lista:
+        print(dados, end=", ")
+    print()
