76bbc3878173793f6fbeff10f8f926a02a1ce5b1
jonrgomes <jonrgomes@gmail.com>
2026-09-10 21:03:56 -0300

    Estrutura_de_dados

--- 23_introducao_a_dicionarios.py
+++ 23_introducao_a_dicionarios.py
@@ -1,1 +1,16 @@
 # Sintaxe básica:
+carros = {}
+
+pessoas = {
+    "nome": "José da Silva",
+    "Idade": 23,
+    "Altura": 1.99,
+    "Peso": 65.4,
+    "Habilitado": True
+}
+
+for chave, valor in pessoas.items():
+    print(f"{chave}: {valor}")
+
+# Acessando um item de um dicionário:
+
