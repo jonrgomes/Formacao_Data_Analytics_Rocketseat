ad4a812d3aa82b012a071ee12a21170d415626d4
jonrgomes <jonrgomes@gmail.com>
2026-09-11 21:12:18 -0300

    Estrutura_de_dados

--- 23_introducao_a_dicionarios.py
+++ 23_introducao_a_dicionarios.py
@@ -1,16 +1,45 @@
 # Sintaxe básica:
 carros = {}
 
+# pessoas = {
+#     "nome": "José da Silva",
+#     "Idade": 23,
+#     "Altura": 1.99,
+#     "Peso": 65.4,
+#     "Habilitado": True
+# }
+
+# for chave, valor in pessoas.items():
+#     print(f"{chave}: {valor}")
+
+# # Acessando um item de um dicionário:
+# print(pessoas["nome"])
+# print(pessoas["Idade"])
+
+# # Não acieta valores duplicados:
+
+# pessoas = {
+#     "nome": "José da Silva",
+#     "Idade": 23,
+#     "Altura": 1.99,
+#     "Peso": 65.4,
+#     "Peso": 78.5, #repete sempre o último valor
+# }
+# for chave, valor in pessoas.items():
+#     print(f"{chave}: {valor}")
+
+# Modificando um item de um dicionário:
 pessoas = {
     "nome": "José da Silva",
     "Idade": 23,
     "Altura": 1.99,
     "Peso": 65.4,
-    "Habilitado": True
 }
 
+pessoas["Peso"] = "2.5"
 for chave, valor in pessoas.items():
-    print(f"{chave}: {valor}")
+    print(f"{chave}, {valor}")
 
-# Acessando um item de um dicionário:
 
+# Tamanho de um dicionário:
+print(len(pessoas))
