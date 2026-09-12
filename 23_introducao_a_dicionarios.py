3c951c3f5f3e1a198dfa98e4b07470fb483bf299
jonrgomes <jonrgomes@gmail.com>
2026-09-12 13:28:47 -0300

    Estrutura_de_dados

--- 23_introducao_a_dicionarios.py
+++ 23_introducao_a_dicionarios.py
@@ -1,32 +1,32 @@
 # Sintaxe básica:
 carros = {}
 
-# pessoas = {
-#     "nome": "José da Silva",
-#     "Idade": 23,
-#     "Altura": 1.99,
-#     "Peso": 65.4,
-#     "Habilitado": True
-# }
+pessoas = {
+    "nome": "José da Silva",
+    "Idade": 23,
+    "Altura": 1.99,
+    "Peso": 65.4,
+    "Habilitado": True
+}
 
-# for chave, valor in pessoas.items():
-#     print(f"{chave}: {valor}")
+for chave, valor in pessoas.items():
+    print(f"{chave}: {valor}")
 
-# # Acessando um item de um dicionário:
-# print(pessoas["nome"])
-# print(pessoas["Idade"])
+# Acessando um item de um dicionário:
+print(pessoas["nome"])
+print(pessoas["Idade"])
 
-# # Não acieta valores duplicados:
+# Não acieta valores duplicados:
 
-# pessoas = {
-#     "nome": "José da Silva",
-#     "Idade": 23,
-#     "Altura": 1.99,
-#     "Peso": 65.4,
-#     "Peso": 78.5, #repete sempre o último valor
-# }
-# for chave, valor in pessoas.items():
-#     print(f"{chave}: {valor}")
+pessoas = {
+    "nome": "José da Silva",
+    "Idade": 23,
+    "Altura": 1.99,
+    "Peso": 65.4,
+    "Peso": 78.5, #repete sempre o último valor
+}
+for chave, valor in pessoas.items():
+    print(f"{chave}: {valor}")
 
 # Modificando um item de um dicionário:
 pessoas = {
--- Pratica_04.py
+++ Pratica_04.py
@@ -1,6 +1,21 @@
-# Integração Python + Excel:
-import pandas as pd
+# Exercício com dicionários:
+""" 
+Objetivo: a partir de vendas = [120, 85, 200, 150, 90, 300, 175], calcular total, média, maior e menor valor.
+"""
 
+vendas = [120, 85, 200, 150, 90, 300, 175]
 
+total = 0 # cria um "acumulador" — uma variável que vai guardar a soma conforme o loop avança. Ela começa em zero pois não existe soma;
+for x in vendas: # percorre a lista item por item. A cada volta, x recebe um valor diferente;
+    total += x # (também pode escrever total += x) 
+print(f"O total em vendas é {total}R$.")
 
+# Calculando a média:
 
+media =  total / len(vendas) # (len) retorna quantos elementos tem a lista
+print(f"A média em vendas é de {media:.2f}R$.")
+
+# A maio e menor venda:
+maior = max(vendas)
+menor = min(vendas)
+print(f"A maior venda nos últimos 7 dias foi {maior}R$, sendo a menor {menor}R$!")
