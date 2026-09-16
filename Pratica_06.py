5b84ae0362626656dc9c69a5eb14b6be5780f11e
jonrgomes <jonrgomes@gmail.com>
2026-09-16 19:50:46 -0300

    Estrutura_de_dados

--- Pratica_06.py
+++ Pratica_06.py
@@ -4,20 +4,20 @@ Você tem a lista frutas = ["maçã", "banana", "uva", "manga"]. Use um for para
 """
 
 # 1 Declarar uma variavel frutas e atribuir uma lista:
-# frutas = ["maça", "banana", "uva", "manga"]
+frutas = ["maça", "banana", "uva", "manga"]
 
-# # Percorrer a lista frutas para e para fruta escrever a fruta e sua posição.
-# posicao = 1
-# for fruta in frutas:
-#     print(posicao, fruta)
-#     posicao += 1
+# Percorrer a lista frutas para e para fruta escrever a fruta e sua posição.
+posicao = 1
+for fruta in frutas:
+    print(posicao, fruta)
+    posicao += 1
 
-# # usar enumerate() (mais "pythônico", combina índice + valor automaticamente)
+# usar enumerate() (mais "pythônico", combina índice + valor automaticamente)
 
-# # Percorrer a lista frutas para e para fruta escrever a fruta e sua posição, usa o enumarate com o inicio em 1 para escrever a posição de cada fruta da lista.
-# for posicao, fruta in enumerate(frutas, start=1):  
-#     print(posicao, fruta)
-#     posicao += 1
+# Percorrer a lista frutas para e para fruta escrever a fruta e sua posição, usa o enumarate com o inicio em 1 para escrever a posição de cada fruta da lista.
+for posicao, fruta in enumerate(frutas, start=1):  
+    print(posicao, fruta)
+    posicao += 1
 
 # Criando uma tabuada:
 
@@ -28,3 +28,12 @@ for tab in tabuada:
     multi = valor * tab
     print(f"{valor} * {tab} = {multi}")
     
+# Criando uma matriz:
+
+dados = ["A", "B", "C", "D", "E", "F"]
+
+for linha in range(1, 51):
+    for coluna in dados:
+        matriz = coluna + str(linha)
+        print(matriz, end=",")
+    print()
