75b2fc241dfb7d682f1da622ce31af1abb2819a9
jonrgomes <jonrgomes@gmail.com>
2026-09-18 20:01:54 -0300

    Estrutura_de_dados

--- Pratica_05.py
+++ Pratica_05.py
@@ -10,3 +10,36 @@ print(venda["preco_unitario"])
 venda_total = venda["quantidade"] * venda["preco_unitario"]
 print(f"A venda total é de {venda_total:.2f}R$!".replace(".", ","))
 
+# Cadastro de Produtos em sistema: 
+
+# produtos = []
+
+while True:
+    nome = input("Nome do produto: ")
+    preco_produto = float(input("Preço do produto: "))
+    
+    novo_cadastro = input("Deseja prosseguir (S/N): ")
+    
+    if novo_cadastro.lower() == "n":
+        break
+    
+    
+# Barbearia, marcando horário: 
+
+barber_cliente = []
+
+while True:
+    nome_cliente = input("Nome do cliente: ")
+    horario_marcado = str(input("Horário marcado: "))
+    
+    dados_agenda = {"nome": nome_cliente, "horario": horario_marcado}
+    barber_cliente.append(dados_agenda)
+    
+    novo_horario = input("Temos uma nova agenda? (Y/N): ")
+    
+    if novo_horario.lower() == "n":
+        break 
+
+for agenda in barber_cliente:
+    print(agenda)
+   
