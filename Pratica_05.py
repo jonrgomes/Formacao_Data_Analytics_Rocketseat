622c1a627832b2723dc30ffb7aca90b72623b664
jonrgomes <jonrgomes@gmail.com>
2026-09-19 19:46:04 -0300

    Estrutura_de_dados

--- Pratica_05.py
+++ Pratica_05.py
@@ -1,45 +1,45 @@
-# Exercício com dicionários:
-"""
-venda individual com várias informações — tipo uma linha de planilha ()
-"""
-venda = {"produto": "Caneta", "quantidade": 10, "preco_unitario": 2.5}
+# # Exercício com dicionários:
+# """
+# venda individual com várias informações — tipo uma linha de planilha ()
+# """
+# venda = {"produto": "Caneta", "quantidade": 10, "preco_unitario": 2.5}
 
-print(venda["quantidade"])
-print(venda["preco_unitario"])
+# print(venda["quantidade"])
+# print(venda["preco_unitario"])
 
-venda_total = venda["quantidade"] * venda["preco_unitario"]
-print(f"A venda total é de {venda_total:.2f}R$!".replace(".", ","))
+# venda_total = venda["quantidade"] * venda["preco_unitario"]
+# print(f"A venda total é de {venda_total:.2f}R$!".replace(".", ","))
 
-# Cadastro de Produtos em sistema: 
+# # Cadastro de Produtos em sistema: 
 
-produtos = []
+# produtos = []
 
-while True:
-    nome = input("Nome do produto: ")
-    preco_produto = float(input("Preço do produto: "))
+# while True:
+#     nome = input("Nome do produto: ")
+#     preco_produto = float(input("Preço do produto: "))
     
-    novo_cadastro = input("Deseja prosseguir (S/N): ")
+#     novo_cadastro = input("Deseja prosseguir (S/N): ")
     
-    if novo_cadastro.lower() == "n":
-        break
+#     if novo_cadastro.lower() == "n":
+#         break
     
     
-# Barbearia, marcando horário: 
+# # Barbearia, marcando horário: 
 
-barber_cliente = []
+# barber_cliente = []
 
-while True:
-    nome_cliente = input("Nome do cliente: ")
-    horario_marcado = str(input("Horário marcado: "))
+# while True:
+#     nome_cliente = input("Nome do cliente: ")
+#     horario_marcado = str(input("Horário marcado: "))
     
-    dados_agenda = {"nome": nome_cliente, "horario": horario_marcado}
-    barber_cliente.append(dados_agenda)
+#     dados_agenda = {"nome": nome_cliente, "horario": horario_marcado}
+#     barber_cliente.append(dados_agenda)
     
-    novo_horario = input("Temos uma nova agenda? (Y/N): ")
+#     novo_horario = input("Temos uma nova agenda? (Y/N): ")
     
-    if novo_horario.lower() == "n":
-        break 
+#     if novo_horario.lower() == "n":
+#         break 
 
-for agenda in barber_cliente:
-    print(agenda)
+# for agenda in barber_cliente:
+#     print(agenda)
    
