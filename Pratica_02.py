3adca8e44d16c1892fe67b718da63ef662c4ab96
jonrgomes <jonrgomes@gmail.com>
2026-09-23 19:44:18 -0300

    Estrutura_de_dados

--- Pratica_02.py
+++ Pratica_02.py
@@ -1,18 +1,18 @@
-# # 1 - Variáveis e Tipos de Dados:
-# produto = 'Geladeira'
-# categoria = 'Eletrodoméstico'
-# quantidade = 100
-# preco_unitario = 2400.99
-# em_estoque = True
+# 1 - Variáveis e Tipos de Dados:
+produto = 'Geladeira'
+categoria = 'Eletrodoméstico'
+quantidade = 100
+preco_unitario = 2400.99
+em_estoque = True
 
-# # 2 Saída de Dados: 
+# 2 Saída de Dados: 
 
-# print('=== Registro de Venda ===')
-# print(f'Produto: {produto}')
-# print(f'Categoria: {categoria}')
-# print(f'Quantidade: {quantidade}')
-# print(f'Preço unitário: R${preco_unitario}')
-# print('-----------------------------------------------')
+print('=== Registro de Venda ===')
+print(f'Produto: {produto}')
+print(f'Categoria: {categoria}')
+print(f'Quantidade: {quantidade}')
+print(f'Preço unitário: R${preco_unitario}')
+print('-----------------------------------------------')
 
 # 3 Entrada de Dados: 
 
@@ -27,7 +27,7 @@ print('---------------------------------------------------')
 # 4 Operadores Aritiméticos: 
 valor_total_da_venda = quantidade * preco_unitario
 valor_desconto = valor_total_da_venda * 0.1
-valor_final = valor_total_da_venda - valor_desconto
+valor_final = float(valor_total_da_venda - valor_desconto)
 print(f'O valor total da venda é R${valor_total_da_venda}!')
 print(f'O desconto concedido é de R${valor_desconto}!')
 print(f'Sua compra fica no valor de R${valor_final:.2f}!')
@@ -35,12 +35,12 @@ print('---------------------------------------------------')
 
 # 5 Operadores Relacionais:
 
-valor_final > 1000
-print('É uma venda de alto valor?')
-quantidade == 0
-print('A venda esta vazia?')
-desconto_percentual >= 50
-print('O desconto é considerado agressivo.')
+precificacao = valor_final > 1000
+print(f"É uma venda de alto valor? {precificacao}")
+variedade = quantidade == 0
+print(f'A venda esta vazia? {variedade}')
+preco_desconto = desconto_percentual >= 50
+print(f'O desconto é considerado agressivo. {preco_desconto}')
 print('---------------------------------------------------')
 
 # Operadores Lógicos:
