f1aa4cb310cb10ef6ef8e4159525df3168a638d9
jonrgomes <jonrgomes@gmail.com>
2026-09-24 20:32:05 -0300

    Estrutura_de_dados

---
+++ 27_introducao_a_sets.py
@@ -0,0 +1,18 @@
+# Sintxe de um set:
+frutas = {"banara", "uva", "laranja"}
+print(frutas)
+print(type(frutas))
+
+# Criar um set vazio:
+frutas1 = set()
+print(type(frutas1))
+
+# Não é possível utilizar valores duplicados em sets:
+numeros = {1, 2, 3, 4, 4, 4, 4, 5, 6, 7, 7, 8}
+frutas = {"banana", "uva", "banana", "maçã"}
+
+print(numeros)
+print(frutas)
+
+# Tamanhpo de um set: 
+
--- tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-# Percorrendo um dicionário aninhado:
-# for chave, valor in cliente.items():
-#     for chave_interna, valor_interno in valor.items():
-#         print(f"{chave_interna}: {valor_interno}")
-#     print()
-    
